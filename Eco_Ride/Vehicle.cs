ff403cbf9ce38c3d125b6df19f328496cf222062
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-05 11:42:18 +0530

    [Prashant] add Vehicle class with encapsulating private variables

--- Eco_Ride/Vehicle.cs
+++ Eco_Ride/Vehicle.cs
@@ -8,7 +8,44 @@ namespace Eco_Ride
     {
         public int VehicleId;
         public string Model;
-        public int BatteryPercentage;
+        private int _batteryPercentage;
+        private string maintenanceStatus;
+        public int BatteryPercentage
+        {
+            get { return _batteryPercentage; }
+            set
+            {
+                if (value < 0)
+                {
+                    _batteryPercentage = 0;
+                }
+                else if (value > 100)
+                {
+                    _batteryPercentage = 100;
+                }
+                else
+                {
+                    _batteryPercentage = value;
+                }
+
+            }
+        }
+        public string MaintenanceStatus
+        {
+            get { return maintenanceStatus; }
+            set
+            {
+                if (value == "Good" || value == "Needs Service" || value == "Out of Service")
+                {
+                    maintenanceStatus = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid maintenance status");
+                }
+            }
+        }
+
 
         public Vehicle(int vehicleId, string model, int batteryPercentage)
         {
