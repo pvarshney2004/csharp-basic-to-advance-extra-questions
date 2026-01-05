6f39d407b5ad5f4056c73391296ad52e4e4e6d37
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-05 12:15:36 +0530

    [Prashant] add EcoRideMain class implementing Main method

---
+++ Eco_Ride/EcoRideMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eco_Ride
+{
+    public class EcoRideMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Eco-Ride Urban Mobility System");
+            Console.WriteLine("-----------------------------------------");
+            List<Vehicle> vehicles = new List<Vehicle>
+            {
+                new ElectricCar(1, "EcoCar Model X", 85, 4, "Good"),
+                new ElectricScooter(2, "EcoScooter Model S", 60, 25, "Needs Service")
+            };
+            double tripDistance = 10.0;
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine($"Vehicle ID: {vehicle.VehicleId}, Model: {vehicle.Model}, Maintainance Status: {vehicle.MaintenanceStatus}, Distance covered: {tripDistance}km");
+                vehicle.CalculateTripCost(tripDistance);
+                Console.WriteLine("------------------------------");
+            }
+        }
+    }
+}
