9e1df545bdb58041fa8ba7594fe9e602aac6c80e
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-05 11:54:39 +0530

    [Prashant] add subclasses ElectricCar and ElectricScooter implementing Inheritance

---
+++ Eco_Ride/ElectricCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eco_Ride
+{
+    public class ElectricCar : Vehicle
+    {
+        private int seatingCapacity;
+        public ElectricCar(int vehicleId, string model, int batteryPercentage, int seatingCapacity, string maintainance)
+            : base(vehicleId, model, batteryPercentage, maintainance)
+        {
+            this.seatingCapacity = seatingCapacity;
+        }
+    }
+
+}
---
+++ Eco_Ride/ElectricScooter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eco_Ride
+{
+    public class ElectricScooter : Vehicle
+    {
+        private int maxSpeed;
+        public ElectricScooter(int vehicleId, string model, int batteryPercentage, int maxSpped, string maintainance)
+            : base(vehicleId, model, batteryPercentage, maintainance)
+        {
+            this.maxSpeed = maxSpped;
+        }
+    }
+}
