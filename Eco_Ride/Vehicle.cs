cd217c5f3bd398fc0b34070a1c6d9816242eeb7f
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-05 11:16:25 +0530

    [Prashant] add Vehicle class

---
+++ Eco_Ride/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eco_Ride
+{
+    public class Vehicle
+    {
+        public int VehicleId;
+        public string Model;
+        public int BatteryPercentage;
+
+        public Vehicle(int vehicleId, string model, int batteryPercentage)
+        {
+            VehicleId = vehicleId;
+            Model = model;
+            BatteryPercentage = batteryPercentage;
+        }
+    }
+}
